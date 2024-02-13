e57173a422a4a8986a0642b2f0167a8ab4e84966
Anton Makushenko <antonmakushenko@iMac-Anton.local>
2024-02-13 22:10:42 +0200

    upd

--- api/routes/sessions.js
+++ api/routes/sessions.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/", getAllSessions);
 router.get("/:id", getSessionById);
 router.post("/", createSession);
-router.post("/:id/join", joinSession);
+router.post("/join", joinSession);
 router.post("/:id/matches", addMatchesToSession);
 
 export default router;
