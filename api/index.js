39c5da5838f859ba7abf6f3b53215c37ddf5a16f
Anton Makushenko <antonmakushenko@iMac-Anton.local>
2024-02-13 19:16:03 +0200

    update

--- api/index.js
+++ api/index.js
@@ -1,41 +1,36 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import helmet from "helmet";
-// import morgan from "morgan";
-// import authRoutes from "./routes/auth.js";
-// import userRoutes from "./routes/users.js";
-//
-// dotenv.config();
-// const app = express();
-// app.use(express.json());
-// app.use(helmet());
-// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(morgan("common"));
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
-//
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-//
-// const PORT = process.env.PORT || 6001;
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-//   })
-//   .catch((error) => console.log(`${error} did not connect`));
-
 import express from "express";
-import { sayHello } from "./function.js";
-const app = express();
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import authRoutes from "../routes/auth.js";
+import userRoutes from "../routes/users.js";
+import { sayHello } from "./function";
 
-app.get("/api", sayHello);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/test", sayHello);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // .then(() => {
+  //   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  // })
+  .catch((error) => console.log(`${error} did not connect`));
 
 module.exports = app;
