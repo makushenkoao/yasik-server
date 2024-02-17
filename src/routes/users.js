07afc0aa1a1a4cc75fd81db71c08c5112a4297fa
Anton Makushenko <antonmakushenko@iMac-Anton.local>
2024-02-18 01:37:48 +0200

    upd

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,6 @@ import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-// router.get("/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 
